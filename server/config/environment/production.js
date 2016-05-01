4397150ff1127b67deb6ae2f95987dd83415ac91
cetrobo <achraf.benaomar@gmail.com>
2016-05-01 15:28:05 +0200

    edite

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,6 +3,6 @@
 module.exports = {
   ip: process.env.IP || undefined,
   mongo: {
-    uri: 'mongodb://localhost/crud'
+    uri: 'mongodb://heroku_t19t7ddd:drvklsnqll5b8h485ihbpl3eql@ds011462.mlab.com:11462/heroku_t19t7ddd'
   }
 };
