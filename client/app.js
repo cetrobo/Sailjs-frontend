285c34833b22b50150ad9dbcc4960edfcf401048
cetrobo <achraf.benaomar@gmail.com>
2016-05-04 13:55:27 +0200

    fix sailsjs.io

--- client/app.js
+++ client/app.js
@@ -4,13 +4,16 @@ angular.module('crud', [
   'ngRoute',
   'angular-jwt',
   'ngSails'
+  
 
 ])
   .config(function ($routeProvider, $locationProvider,$sailsProvider) {
     //console.log($locationProvider);
-     //$sailsProvider.url = 'http://foo.bar';
+     $sailsProvider.url = 'https://sails-backoffice.herokuapp.com/';
      //console.log($sailsProvider.url);
-     console.log($sailsProvider);
+
+    
+     //console.log($sailsProvider);
     $routeProvider
       .otherwise({
         redirectTo: '/'
--- client/views/signup/signup.controller.js
+++ client/views/signup/signup.controller.js
@@ -1,8 +1,29 @@
 'use strict';
 
 angular.module('crud')
-  .controller('SignupCtrl', function () {
+  .controller('SignupCtrl', function ($scope,$sails) {
 
-  			
+  
+  			//console.log()
+  		$scope.user = [];
 
-  	  });	
+
+    // Using .success() and .error()
+   $scope.create=function(){
+
+
+    $sails.post("/api/user")
+      .success(function (data, status, headers, jwr) {
+        console.log(data);
+        //$scope.user=data;
+      })
+      .error(function (data, status, headers, jwr) {
+        
+       //console.log(status);
+        alert('Houston, we got a problem!');
+      });		
+
+};
+
+});	
+
