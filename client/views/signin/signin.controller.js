67ac71db63463389f02c158e8c5640c67fa0fa6c
Younes <younes@interactive-object.com>
2016-05-20 13:14:33 +0200

    Update signin.controller.js

--- client/views/signin/signin.controller.js
+++ client/views/signin/signin.controller.js
@@ -5,19 +5,19 @@ angular.module('crud')
 
 
   	$scope.signin = function(){
-      User.setUser(user);
-    var data={ email:$scope.email,password:$scope.password}
-       var user=User.signin(data);
-        //console.log(typeof user);
-     user.success(function (data) {
-                    //console.log("token signin",data);
-                    var token=data.token;
-                localStorage.setItem('id_token', token);
-                    $location.path("/todo");
-      })
-        .error(function (data) {
-                    //Do whatever is needed
-                 //console.log(data);
+      
+      var data={ email:$scope.email,password:$scope.password};
+      
+       User.signin(data).success(function (data) {
+          // save the user data in the service 
+          User.setUser(data);
+          var token=data.token;
+          // TODO save the token whitin the User service instead of here
+          localStorage.setItem('id_token', token);
+          $location.path("/todo");
+      }).error(function (data) {
+          // TODO add alter here
+          //console.log(data);
      });  
     }	
 
